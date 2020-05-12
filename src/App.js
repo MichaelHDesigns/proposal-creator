569eb202407a8641b9d1111b47a8441b1be1834b
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-05-12 00:07:56 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -510,7 +510,7 @@ class App extends Component
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Energi Proposal Creator</h1>
+          <h1 className="App-title">HTH Proposal Creator</h1>
         </header>
         <PrepareForm {...prepareform_props} />
       </div>
